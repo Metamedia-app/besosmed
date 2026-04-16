bcd28b153ca59d5ad2d7734cbe27af563f3b12ed
edy2209 <edisyafrianto45.com>
2026-04-16 08:05:29 +0700

    finish delete post bug be

--- src/controllers/posts/deletePost.js
+++ src/controllers/posts/deletePost.js
@@ -1,11 +1,14 @@
 import Post from '../../models/Post.js';
+import Comment from '../../models/Comment.js';
+import Like from '../../models/Like.js';
+import Notification from '../../models/Notification.js';
 import { deleteFile } from '../../services/r2Service.js';
 
 export async function deletePost(request, reply) {
   const userId = request.user.id;
   const { id } = request.params;
 
-  const post = await Post.findOne({ _id: id, is_deleted: false });
+  const post = await Post.findOne({ _id: id });
 
   if (!post) {
     return reply.status(404).send({ success: false, message: 'Postingan tidak ditemukan.' });
@@ -15,13 +18,22 @@ export async function deletePost(request, reply) {
     return reply.status(403).send({ success: false, message: 'Kamu tidak punya akses untuk menghapus postingan ini.' });
   }
 
-  // Soft delete dulu, hapus media dari R2 di background
-  post.is_deleted = true;
-  await post.save();
-
-  // Hapus media dari R2 (non-blocking)
+  // 1. Bersihkan Media dari R2 (non-blocking)
   if (post.media?.length > 0) {
     Promise.all(post.media.map((m) => deleteFile(m.key))).catch(() => {});
+  }
+
+  // 2. Cascade Delete (Pembersihan Total di Database)
+  try {
+    await Promise.all([
+      Post.deleteOne({ _id: id }),           // Hapus Post
+      Comment.deleteMany({ post_id: id }),   // Hapus seluruh komentar post ini
+      Like.deleteMany({ post_id: id }),      // Hapus seluruh Like post ini
+      Notification.deleteMany({ post_id: id }) // Hapus notifikasi terkait post ini
+    ]);
+  } catch (error) {
+    request.log.error(error);
+    // Kita tetap lanjut mengirim sukses karena post utama biasanya sudah terhapus
   }
 
   return reply.send({
